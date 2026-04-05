524ddd017c8d0529c21b5350076bed9a8f03752e
KirtikaPuniani <75201661+KirtikaPuniani@users.noreply.github.com>
2026-04-05 18:35:20 +0530

    sql code

--- SQL/Create-Tables.sql
+++ SQL/Create-Tables.sql
@@ -1,1 +1,8 @@
-
+create table IF NOT EXISTS weather_data(
+    ID serial primary key,
+    City varchar(50),
+    Temperature float,
+    Wind_Speed float,
+    Humidity float,
+    Date_Time timestamp default current_timestamp
+)
